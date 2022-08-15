6b55015a7adf972c7faa27cb7190442967ca840c
sakshamsang <sakshamsang97@gmail.com>
2022-08-15 14:17:06 -0400

    Initial commit

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ app.use('/createUser', createUserValidation);
 app.use('/getUser', loginValidation);
 app.use('/updateUser', updateUserValidation);
 
-
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
